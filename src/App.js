9c34e690c068329f6800fe66d697dfd5af395d2b
MLhimanshu <materialuihimanshu@gmail.com>
2023-04-24 15:11:54 +0530

    he

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import './App.css';
 import Header from "./component/layout/Header/Header.js";
 
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import WebFont from 'webfontloader';
 import { useEffect } from 'react';
 import Footer from './component/layout/Footer/Footer';
+import Home from "./component/Home/Home.js";
 
 
 
@@ -21,6 +22,7 @@ function App() {
   return (
     <Router>
       <Header/>
+      <Route exact path='/' Component={Home} />
       <Footer/>
     </Router>
    
